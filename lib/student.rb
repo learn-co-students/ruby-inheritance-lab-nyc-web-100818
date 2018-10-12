5744fd042c078e0c97b5596e13a4ee106f3900ea
magic-onion <alexander.m.truong@gmail.com>
2018-10-12 13:38:12 -0400

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(facts)
+    @knowledge << facts
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
