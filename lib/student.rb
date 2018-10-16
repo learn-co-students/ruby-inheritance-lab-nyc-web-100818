95d26ea3bb41081d0457d0bbb6d684efca2eafa2
Rexman17 <a.stone1794@gmail.com>
2018-10-16 13:07:41 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative "./user.rb"
 
-end
+class Student < User
+  attr_reader :knowledge # just need a reader bc we'll need custom setter to fill this
+
+  def initialize
+    # @name = name
+    @knowledge = [] # why don't we need an attr_accessor for this?
+  end
+
+  def learn(knowledge_string)
+    @knowledge << knowledge_string
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+end # end of Student class
